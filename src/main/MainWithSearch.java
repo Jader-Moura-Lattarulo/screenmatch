2b63c4bc9732a3b86ea671f6e845226fafdf14f6
Jader Moura <jader.dev.moura@gmail.com>
2024-11-06 16:01:31 -0300

    adding a API

---
+++ src/main/MainWithSearch.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var search = scanner.nextLine();
+
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=79863d6d";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
